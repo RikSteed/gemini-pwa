1b0c8c221f09f417e11098fdb62b18e532180602
Riccardo Francesco Croce <riccardocroce129@outlook.com>
2021-08-12 16:55:15 +0200

    adding PWA features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // Angular Material Imports
 import { MatSliderModule } from '@angular/material/slider';
-import { MatButtonModule } from '@angular/material/button'
+import { MatButtonModule } from '@angular/material/button';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment'
 
 @NgModule({
   declarations: [
@@ -19,7 +21,13 @@ import { MatButtonModule } from '@angular/material/button'
     BrowserAnimationsModule,
     // Angular Material Modules
     MatSliderModule,
-    MatButtonModule
+    MatButtonModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
