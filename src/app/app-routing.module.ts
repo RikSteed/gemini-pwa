f856ce97b858565e112b842960e9719029854289
Riccardo Francesco Croce <riccardocroce129@outlook.com>
2021-08-30 12:23:57 +0200

    Initial Commit - Redone Import and Deleting old files from template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { Page1Component } from './page1/page1.component';
-import { Page2Component } from './page2/page2.component';
 
 const routes: Routes = [
   {path: '', children:[
-    {path: 'page1', component: Page1Component},
-    {path: 'page2', component: Page2Component}
   ]}
 ];
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,13 +23,11 @@ describe('AppComponent', () => {
   it(`should have as title 'rikMaterialBootstrapTemplate'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('rikMaterialBootstrapTemplate');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('rikMaterialBootstrapTemplate app is running!');
   });
 });
